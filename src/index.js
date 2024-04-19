794ff8c318c8c8e7fb6e8425df43ba8e684f4c2f
WinterSunset95 <wintersunset95@gmail.com>
2024-04-19 22:54:07 +0530

    changed a config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const app = express()
 const http = require('http');
 const server = http.createServer(app);
 
+const port = process.env.PORT || 3000
+
 const io = require('socket.io')(server, {
 	cors: {
 		origin: "*",
@@ -21,6 +23,6 @@ io.on('connection', (socket) => {
 	io.emit('message', 'A user connected')
 })
 
-server.listen(3000, () => {
-	console.log("Listening on port 3000")
+server.listen(port, () => {
+	console.log("Listening on port: " + port)
 })
